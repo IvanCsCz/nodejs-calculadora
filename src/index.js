46c64cf732106424c0c4d4c63001b832de0850be
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-05-22 19:07:28 -0600

    Enter captured

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import { printHello } from '#Lib/hello.js';
+import { promptQuestion } from '#Lib/promptQuestion.js';
 
-printHello();
+(async () => {
+	// 1 Capturar la entrada del usuario por consola
+	const userAnswer = await promptQuestion('Introduce tu operacion: ');
+	console.log(userAnswer);
+	// 2 Validar la entrada y separar las partes de la misma en operandos y operador
+	// 3 Realizar la operacion
+	// 4 Mostrar resultados por consola
+})();
